85b05dd631f1bf3b4ca4429b48cada630c6b5e63
Tanner Olsen <tanner.olsen@student.sl.on.ca>
2019-09-16 15:10:56 -0400

    Bobblehead wars Chapter 3 End

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checking to see if the player is firing and if they're not firing.
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsInvoking("FireBullet"))
+            {
+                InvokeRepeating("FireBullet", 0f, 0.1f);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("FireBullet");
+        }
+
+    }
+
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+    void FireBullet()
+    {
+        // Creates instance of bullet prefab
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        // Where the bullet launches from
+        bullet.transform.position = launchPosition.position;
+        // The bullet is a constant speed thanks to the rigid body
+        bullet.GetComponent<Rigidbody>().velocity =
+        transform.parent.forward * 100;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets the positive and negative inputs to determine where the player moves
+        Vector3 pos = transform.position;
+        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        // Sets the position every frame
+        transform.position = pos;
+    }
+}
